0275d7e528573b3338e842690cf19c86b723c83c
pjliddy <pjliddy@gmail.com>
2017-06-20 08:38:54 -0400

    Added afterModel() event to theme route
    Stores current theme in local storage to access when theme is selected
    to be rendered. Gathers variable array and passes it in a message to
    the layout iframe.

--- app/routes/theme.js
+++ app/routes/theme.js
@@ -2,25 +2,31 @@ import Ember from 'ember';
 import { storageFor } from 'ember-local-storage';
 
 export default Ember.Route.extend({
-
+  // use ember-local-storage to access current theme object
   themeStore: storageFor('theme'),
 
+  // save current theme to ember-local-storage when model is returned
   model (params) {
-    const theme = this.get('store').findRecord('theme', params.theme_id);
-    this.set('themeStore.currentTheme', theme);
+    // const theme = this.get('store').findRecord('theme', params.theme_id);
+    // this.set('themeStore.currentTheme', theme);
 
-    return theme;
+    return this.get('store').findRecord('theme', params.theme_id);
+  },
+
+  afterModel (theme) {
+    // store current model in ember-local-storage
+    this.set('themeStore.currentTheme', theme);
   },
 
   init () {
     this._super();
+
     // listen to iFrame with layout
     // window.addEventListener("message", this.handleMessage, false);
-
     window.addEventListener("message", (e) => {
       const currentTheme = this.get('themeStore.currentTheme');
       const vars = currentTheme.get('vars');
-      this.handleMessage(e, vars);
+      this.handleMessage(e, currentTheme);
     }, false);
 
   },
@@ -53,6 +59,7 @@ export default Ember.Route.extend({
      switch (e.data.message) {
        case 'document-ready':
         console.log('message: layout document-ready:', data);
+        this.renderTheme(data);
         break;
      }
    }
